823085c6d2f9bcd94d96cb9e7bec7eb9940c8fa4
Joacim Åström <joacim.astrom@schibsted.com>
2024-09-30 23:44:35 +0200

    With page export

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { DropzoneInput } from "@/components/DropzoneInput";
 import Items from "@/components/Items/Items";
 import { MaxWidth } from "@/components/MaxWidth";
 
-export const Home = () => {
+const Home = () => {
   return (
     <MaxWidth className="flex flex-col gap-2 md:gap-4 items-center w-full">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center prose">
