f8dc4d4d2e9537a0d8d844ff43d1731cf6500b78
Joacim Åström <joacim.astrom@schibsted.com>
2024-10-02 09:02:14 +0200

    Fix stupid mistake

--- components/auth/AuthDialog.tsx
+++ components/auth/AuthDialog.tsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
-import Sign inDialog from "./Sign inDialog";
-// components/Sign inDialog.js
+import LoginDialog from "./LoginDialog";
+// components/LoginDialog.js
 import { Button } from "../ui/button";
 import { Dialog, DialogTrigger } from "../ui/dialog";
 
 export const AuthDialog = () => {
-  const [showSign in, setShowSign in] = useState(true);
+  const [showLogin, setShowLogin] = useState(true);
 
-  const openSign in = () => setShowSign in(true);
-  const openRegister = () => setShowSign in(false);
+  const openLogin = () => setShowLogin(true);
+  const openRegister = () => setShowLogin(false);
 
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button>Sign in</Button>
       </DialogTrigger>
-      <Sign inDialog openRegister={openRegister} />
+      <LoginDialog openRegister={openRegister} />
     </Dialog>
   );
 };
--- components/auth/LoginDialog.tsx
+++ components/auth/LoginDialog.tsx
@@ -1,30 +1,11 @@
-// components/Sign inDialog.js
+// components/LoginDialog.js
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { toast } from "sonner";
 import GoogleIcon from "../icons/Google";
 import { Button } from "../ui/button";
 import { DialogContent, DialogHeader } from "../ui/dialog";
 
-export default function Sign inDialog({ openRegister }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const router = useRouter();
-
-  const credentialsSign in = async (e) => {
-    e.preventDefault();
-    const res = await signIn("credentials", {
-      redirect: false,
-      email,
-      password,
-    });
-    if (!res?.ok) {
-      toast.error(res?.error);
-    }
-  };
-
-  const googleSign in = async () => {
+export default function LoginDialog() {
+  const googleLogin = async () => {
     await signIn("google");
   };
 
@@ -33,7 +14,7 @@ export default function Sign inDialog({ openRegister }) {
       <DialogHeader>
         <h3 className="text-xl font-semibold">Logga in</h3>
       </DialogHeader>
-      <Button onClick={googleSign in} variant="outline">
+      <Button onClick={googleLogin} variant="outline">
         <GoogleIcon className="mr-2" />
         Sign in with Google
       </Button>
